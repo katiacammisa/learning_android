99801b5fa8502ab9230a0fd7193cc5584f463f48
katiacammisa <katia.cammisa@ing.austral.edu.ar>
2025-03-11 23:27:35 -0300

    Initialize project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.austral.learning_android"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.austral.learning_android"
