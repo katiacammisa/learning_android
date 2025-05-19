8cf8b794d26e02f5d230f9b6e820738d47846be6
katiacammisa <katia.cammisa@ing.austral.edu.ar>
2025-05-19 13:05:27 -0300

    Set dark theme

--- app/src/main/java/com/austral/learning_android/ui/theme/Theme.kt
+++ app/src/main/java/com/austral/learning_android/ui/theme/Theme.kt
@@ -1,25 +1,16 @@
 package com.austral.learning_android.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Black,
+    secondary = White,
+    tertiary = Grey,
+    background = White,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -32,19 +23,19 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+private val DarkColorScheme = darkColorScheme(
+    primary = White,
+    secondary = Black,
+    tertiary = LightGrey,
+    background = DarkBlue,
+)
+
 @Composable
 fun LearningAndroidTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
